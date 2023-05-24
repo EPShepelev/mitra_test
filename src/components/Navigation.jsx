add33321ed8c5376def82cd914712228710a80c5
shepelevep <shepelevep@answer-42.ru>
2023-05-24 13:18:31 +0300

    доработал навигацию

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,41 +3,43 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { Link } from 'react-router-dom'
 import myAvatar from '../static/my_avatar.jpg'
 import Avatar from '../components/Avatar'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Navigation = () => {
-    return (
-        <>
-          {[false].map((expand) => (
-            <Navbar key={expand} bg="light" expand={expand} className="mb-3">
-              <Container fluid>
-                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                <Navbar.Offcanvas
-                  id={`offcanvasNavbar-expand-${expand}`}
-                  aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                  placement="start"
-                >
-                  <Offcanvas.Header closeButton>
-                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                      <Avatar avatar={myAvatar} />
-                      <p>Шепелев Евгений</p>
-                      <p>shepelev.ep@yandex.ru</p>
-                    </Offcanvas.Title>
-                  </Offcanvas.Header>
-                  <Offcanvas.Body>
-                    <Nav className="justify-content-end flex-grow-1 pe-3">
-                      <Link to='/'>Список постов</Link>
-                      <Link to='/about'>Обо мне</Link>
-                    </Nav>
-                  </Offcanvas.Body>
-                </Navbar.Offcanvas>
-              </Container>
-            </Navbar>
-          ))}
-        </>
-      );
+  return (
+    <Navbar expand={false} className="mb-2">
+      <Container fluid>
+        <Navbar.Toggle /> 
+          <Navbar.Offcanvas placement="start">
+            <Offcanvas.Header className="align-items-start" closeButton>
+              <Offcanvas.Title className="d-flex gap-2 align-items-center justify-content-center">
+                <Avatar avatar={myAvatar} />
+                  <div>
+                    <p className="d-flex mb-0">Шепелев Евгений</p>
+                    <p className="d-flex mb-0">shepelev.ep@yandex.ru</p>
+                  </div>
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Item>
+                  <LinkContainer to="/">
+                    <Nav.Link>Список постов</Nav.Link>
+                  </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                  <LinkContainer to='/about'>
+                    <Nav.Link>Обо мне</Nav.Link>
+                  </LinkContainer>
+                </Nav.Item>
+               </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navigation
